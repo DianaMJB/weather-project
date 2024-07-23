6c8a0dda82a9b28cedfa9ead3f3f375dcbfa48f3
diana <diana.jimenezjb@gmail.com>
2024-07-23 16:34:52 -0400

    added handle search function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello world");
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#item-search");
+  console.log(searchInput);
+  let cityElement = document.querySelector("#city");
+  let capitalizedCity = `${searchInput.value
+    .charAt(0)
+    .toUpperCase()}${searchInput.value.slice(1).toLowerCase()}`;
+  cityElement.innerHTML = capitalizedCity;
+}
+
+let searchForm = document.querySelector("#search-engine");
+searchForm.addEventListener("submit", handleSearch);
